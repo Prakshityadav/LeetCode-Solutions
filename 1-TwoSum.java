2e289369d1ca3630e01c9f5b77148e5ff0baad90
prakshityadav <prakshityadav07@gmail.com>
2025-08-15 22:42:44 +0530

    imported HashMap

--- 1-TwoSum.java
+++ 1-TwoSum.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> pairIdx = new HashMap<>();
+        HashMap<Integer, Integer> pairIdx = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];

acf06cd9e163cf745216afda84c0885f5316797f
prakshityadav <prakshityadav07@gmail.com>
2025-08-15 08:56:29 +0530

    342: Power of Four

---
+++ 342-PowerofFour.java
@@ -0,0 +1,10 @@
+// 342: Power of Four
+
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==1){
+            return true;
+        }
+        return n>0 && (n&(n-1))==0 && (n-1)%3==0;
+    }
+}

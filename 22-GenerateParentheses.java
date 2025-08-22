9e5276b12a245caf92f7462c63077d7a0041e63a
prakshityadav <prakshityadav07@gmail.com>
2025-08-22 20:29:33 +0530

    Generate Parantheses

---
+++ 22-GenerateParentheses.java
@@ -0,0 +1,22 @@
+// 22 : Generate Parentheses
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result=new ArrayList<>();
+
+        recursion(0,0,"",n,result);
+        return result;
+    }
+    void recursion(int open,int close,String str,int n,List<String> result){
+        if( open==close&&open+close==n*2){
+            result.add(str);
+            return;
+        }
+
+        if(open<n){
+            recursion(open+1,close,str+ "(",n,result);
+        }
+        if(close<open){
+            recursion(open,close+1,str+ ")",n,result);
+        }
+    }
+}

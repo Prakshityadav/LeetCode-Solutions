918a79dadf451eadccc264679fa8dc659fa3d120
prakshityadav <prakshityadav07@gmail.com>
2025-09-18 18:59:59 +0530

    Maximum Repeating Substring Solved

---
+++ 1668-MaximumRepeatingSubstring.java
@@ -0,0 +1,16 @@
+// 1668 : Maximum Repeating Substring
+
+class Solution {
+    public int maxRepeating(String sequence, String word) {
+        int count = 0;
+        String repeated = word;
+        
+        while (sequence.contains(repeated)) {
+            count++;
+            repeated += word;
+        }
+        
+        return count;
+    }
+}
+

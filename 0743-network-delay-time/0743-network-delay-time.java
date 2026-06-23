c016e685a7e9815b34c01e6f580e9cac191306dc
prakshityadav <72177793+Prakshityadav@users.noreply.github.com>
2026-06-23 20:08:23 +0530

    Time: 15 ms (31.12%), Space: 48.7 MB (86.54%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -0,0 +1,51 @@
+class Solution {    
+    int[] dijkstra(int src, int n, ArrayList<ArrayList<int[]>> adj) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        int[] time = new int[n];
+        Arrays.fill(time, Integer.MAX_VALUE);
+        
+        time[src] = 0; 
+        pq.offer(new int[]{src, 0});
+        
+        while (!pq.isEmpty()) {
+            int[] pair = pq.poll();
+            int u = pair[0];
+            int d = pair[1];
+            
+            if (d > time[u]) continue;
+            
+            for (int[] neighbour : adj.get(u)) {
+                int v = neighbour[0];
+                int w = neighbour[1];
+                
+                if (time[u] + w < time[v]) {
+                    time[v] = time[u] + w;   
+                    pq.offer(new int[]{v, time[v]});
+                }
+            }
+        }
+        return time;
+    }
+    public int networkDelayTime(int[][] times, int n, int k) {
+        ArrayList<ArrayList<int[]>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        
+        for (int[] t : times) { 
+            int u = t[0] - 1;
+            int v = t[1] - 1;
+            int w = t[2];
+            
+            adj.get(u).add(new int[]{v, w});
+        }
+        int[] minTime = dijkstra(k - 1, n, adj);  
+        
+        int res = Integer.MIN_VALUE;
+        for (int t : minTime) {
+            if (t == Integer.MAX_VALUE) return -1;  
+            res = Math.max(res, t);
+        }
+        return res;
+    }
+}

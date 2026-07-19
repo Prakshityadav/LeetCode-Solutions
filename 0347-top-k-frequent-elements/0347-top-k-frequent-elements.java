ce9a68e63d2c76ed57d35ec56df1b541e9ad67e6
prakshityadav <72177793+Prakshityadav@users.noreply.github.com>
2026-07-19 22:39:21 +0530

    Time: 10 ms (98.06%), Space: 47.7 MB (32.27%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,22 +1,70 @@
+// Using Bucket sort
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+
+        // Step 1: Count frequencies
         HashMap<Integer, Integer> freq = new HashMap<>();
 
         for (int num : nums) {
             freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
 
-        PriorityQueue<Map.Entry<Integer, Integer>> pq =
-                new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        // Step 2: Create buckets
+        List<Integer>[] bucket = new ArrayList[nums.length + 1];
 
-        pq.addAll(freq.entrySet());
+        for (int key : freq.keySet()) {
+            int frequency = freq.get(key);
 
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+
+            bucket[frequency].add(key);
+        }
+
+        // Step 3: Collect top k elements
         int[] ans = new int[k];
+        int index = 0;
 
-        for (int i = 0; i < k; i++) {
-            ans[i] = pq.poll().getKey();
+        for (int i = bucket.length - 1; i >= 0 && index < k; i--) {
+
+            if (bucket[i] != null) {
+
+                for (int num : bucket[i]) {
+
+                    ans[index++] = num;
+
+                    if (index == k)
+                        break;
+                }
+            }
         }
 
         return ans;
     }
 }
+
+
+                        // Using PriorityQueue
+// class Solution {
+//     public int[] topKFrequent(int[] nums, int k) {
+//         HashMap<Integer, Integer> freq = new HashMap<>();
+
+//         for (int num : nums) {
+//             freq.put(num, freq.getOrDefault(num, 0) + 1);
+//         }
+
+//         PriorityQueue<Map.Entry<Integer, Integer>> pq =
+//                 new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+
+//         pq.addAll(freq.entrySet());
+
+//         int[] ans = new int[k];
+
+//         for (int i = 0; i < k; i++) {
+//             ans[i] = pq.poll().getKey();
+//         }
+
+//         return ans;
+//     }
+// }

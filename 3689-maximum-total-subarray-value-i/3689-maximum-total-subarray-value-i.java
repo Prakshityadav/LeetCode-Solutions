d0f28ae52066084f1649fdad78b4156b41b36cd0
prakshityadav <72177793+Prakshityadav@users.noreply.github.com>
2026-08-09 11:31:07 +0530

    Time: 1 ms (100%), Space: 62.2 MB (52.85%) - LeetHub

---
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+
+        for(int num:nums){
+            min=Math.min(min,num);
+            max=Math.max(num,max);
+        }
+        return (long)(max-min)*k;
+    }
+}

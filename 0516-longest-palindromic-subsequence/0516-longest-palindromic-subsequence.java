b1ec95f67a523aef7c8751212e6ff3edf118bcb3
prakshityadav <72177793+Prakshityadav@users.noreply.github.com>
2026-08-28 23:42:13 +0530

    Time: 63 ms (18.3%), Space: 63.7 MB (62.6%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,27 +1,63 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        int n = s.length();
-        int[][] memo = new int[n][n];
-      
-        for (int[] row : memo) {
-            Arrays.fill(row, -1);
+        String rev=new StringBuilder(s).reverse().toString();
+        int n=s.length();
+        int[][] dp=new int[n+1][n+1];
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(s.charAt(i-1)==rev.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
         }
-      
-        return lpsRec(s, 0, n - 1, memo);
+        return dp[n][n];
     }
-    static int lpsRec(String s, int lo, int hi,int[][] memo){
-      
-        if (lo == hi)
-            return 1;
-        if (s.charAt(lo) == s.charAt(hi) && lo + 1 == hi)
-            return 2;
+}
+// To get palindromic String
+class Solution1 {
 
-        if (memo[lo][hi] != -1)
-            return memo[lo][hi];
+    public String longestPalindromeSubseq(String s) {
 
-        if (s.charAt(lo) == s.charAt(hi))
-            return memo[lo][hi]= lpsRec(s, lo + 1, hi - 1, memo) + 2;
-      
-        return memo[lo][hi]= Math.max(lpsRec(s, lo, hi - 1, memo),lpsRec(s, lo + 1, hi, memo));
+        String rev = new StringBuilder(s).reverse().toString();
+        int n = s.length();
+
+        int[][] dp = new int[n + 1][n + 1];
+
+        // Create DP table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if (s.charAt(i - 1) == rev.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        // Backtracking to get the string
+        StringBuilder str = new StringBuilder();
+
+        int i = n;
+        int j = n;
+
+        while (i > 0 && j > 0) {
+
+            if (s.charAt(i - 1) == rev.charAt(j - 1)) {
+                str.append(s.charAt(i - 1));
+                i--;
+                j--;
+            } 
+            else if (dp[i - 1][j] >= dp[i][j - 1]) {
+                i--;
+            } 
+            else {
+                j--;
+            }
+        }
+
+        return str.reverse().toString();
     }
 }

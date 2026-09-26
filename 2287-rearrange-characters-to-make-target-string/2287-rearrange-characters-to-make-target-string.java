db9699745c7d343a6dd4b32b9e853a24b54bba8a
prakshityadav <72177793+Prakshityadav@users.noreply.github.com>
2026-09-26 17:32:08 +0530

    Time: 0 ms (100%), Space: 42.7 MB (81.67%) - LeetHub

---
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int rearrangeCharacters(String s, String target) {
+
+        int[] sCount = new int[26];
+        int[] targetCount = new int[26];
+
+        for (char c : s.toCharArray()) {
+            sCount[c - 'a']++;
+        }
+
+        for (char c : target.toCharArray()) {
+            targetCount[c - 'a']++;
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < 26; i++) {
+            if (targetCount[i] > 0) {
+                min = Math.min(min, sCount[i] / targetCount[i]);
+            }
+        }
+
+        return min;
+    }
+}
